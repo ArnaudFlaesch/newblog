91fbc1cb7bdc39cfa8e15e8e52d4a6fd4f99f4d5
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-10-10 22:39:47 +0200

    cr

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@
 const nextConfig = {
   basePath: '/newblog',
   output: 'export',
-  reactStrictMode: true // Recommended for the `pages` directory, default in `app`.
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    localeDetection: false
+  },
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
