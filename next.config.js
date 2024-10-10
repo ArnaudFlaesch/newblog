05c2395fe92f7d4692a40cb59a9457cfdbd52fed
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-10-10 22:50:54 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: '/newblog',
+  // basePath: '/newblog',
   output: 'export',
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  i18n: {
-    locales: ['fr', 'en'],
-    defaultLocale: 'fr',
-    localeDetection: false
-  },
-  trailingSlash: true
+  reactStrictMode: true // Recommended for the `pages` directory, default in `app`.
 };
 
 module.exports = nextConfig;
