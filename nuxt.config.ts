5c50e75ec0fad74d134d25e90353080c63d44c48
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-10-21 21:34:05 +0200

    title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'SportsWire - Up-to-date Sports News and Analysis App',
+      title: title,
       meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
